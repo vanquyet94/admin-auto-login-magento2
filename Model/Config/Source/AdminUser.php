1183a3428ef829447bdc4786ef3e9ba25b866ddf
vanquyet94 <vanquyet94@gmail.com>
2018-02-05 23:12:59 +0700

    update source

---
+++ Model/Config/Source/AdminUser.php
@@ -0,0 +1,64 @@
+<?php
+namespace VoolaTech\AdminAutoLogin\Model\Config\Source;
+
+class AdminUser implements \Magento\Framework\Option\ArrayInterface
+{
+
+    /**
+     * @var \Magento\User\Model\ResourceModel\User\Collection
+     */
+    private $userCollection;
+
+    /**
+     * @var array
+     */
+    private $users;
+
+    public function __construct(\Magento\User\Model\ResourceModel\User\Collection $userCollection)
+    {
+        $this->userCollection = $userCollection;
+    }
+
+    /**
+     * Get options in "key-value" format
+     *
+     * @param bool $includeEmptyChoice
+     * @return array
+     */
+    public function toArray($includeEmptyChoice = true)
+    {
+        if (is_null($this->users)) {
+            $this->users = [];
+
+            if ($includeEmptyChoice) {
+                $this->users[''] = __('-- Default behavior --');
+            }
+
+            $this->userCollection->addFieldToFilter('is_active', true);
+            $this->userCollection->addOrder('username', \Magento\Framework\Data\Collection::SORT_ORDER_ASC);
+
+            foreach ($this->userCollection as $user) {
+                $this->users[$user->getUsername()] = $user->getName() . ' (' . $user->getUsername() . ')';
+            }
+        }
+
+        return $this->users;
+    }
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $users = [];
+
+        foreach ($this->toArray() as $value => $label) {
+            $users[] = ['value' => $value, 'label' => $label];
+        }
+
+        return $users;
+    }
+
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'VoolaTech_AdminAutoLogin',
+    __DIR__
+);
